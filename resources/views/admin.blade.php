57ea59451b2dd111f5fbd7d76df7778f4d6991e6
IZIDIA <ctalker-n@mail.ru>
2022-02-20 18:15:58 +0500

    add `bootstrap 5`

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,48 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BOOTSTRAP</title>
+    <link href="{{ asset('css/app_boots.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Колонка ряда</div>
+          </div>
+        </div>
+      </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin', function () {
+    return view('admin');
+})->middleware(['auth'])->name('admin');
