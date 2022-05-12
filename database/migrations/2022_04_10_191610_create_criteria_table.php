586a108355e968393834e779976ffb9fb46b40e7
IZIDIA <ctalker-n@mail.ru>
2022-05-12 14:00:31 +0500

    release `criteria` table

--- database/migrations/2022_04_10_191610_create_criteria_table.php
+++ database/migrations/2022_04_10_191610_create_criteria_table.php
@@ -16,14 +16,34 @@ return new class extends Migration
 	{
 		Schema::create('criteria', function (Blueprint $table) {
 			$table->id();
+			$table->boolean('windows_key');
+			$table->boolean('ethernet');
+			$table->boolean('gpu_install');
 			$table->integer('max_temp_cpu');
 			$table->integer('max_temp_gpu');
+			$table->integer('max_load_cpu');
+			$table->integer('max_load_gpu');
+			$table->integer('min_cores_count');
+			$table->integer('min_ram_size');
+			$table->json('required_active_processes')->nullable();
+			$table->json('required_installed_programs')->nullable();
+			$table->json('required_autoload_programs')->nullable();
 			$table->timestamps();
 		});
 		//Одна строка на всю таблицу.
 		$criterion = new Criterion();
+		$criterion->windows_key = true;
+		$criterion->ethernet = true;
+		$criterion->gpu_install = true;
 		$criterion->max_temp_cpu = 60;
 		$criterion->max_temp_gpu = 60;
+		$criterion->max_load_cpu = 80;
+		$criterion->max_load_gpu = 80;
+		$criterion->min_cores_count = 4; //Logical
+		$criterion->min_ram_size = 4; //GB
+		$criterion->required_active_processes = json_encode(['GoogleUpdate']);
+		$criterion->required_installed_programs = json_encode(['Kaspersky']);
+		$criterion->required_autoload_programs = json_encode(['tvncontrol']);
 		$criterion->save();
 	}
 
